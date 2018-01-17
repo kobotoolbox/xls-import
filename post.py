a5ccca2290ab487c359b7bffbabbe4c0ec83a81d
Marjorie Roswell <mroswell+github@gmail.com>
2018-01-16 19:04:47 -0500

    add uuid to log file

--- post.py
+++ post.py
@@ -4,16 +4,17 @@ import requests
 import glob
 from datetime import datetime
 import csv
+import re
 
 host = 'kc.kobotoolbox.org'
 homedir = os.path.expanduser('~')
-info = netrc.netrc(homedir + "/.netrc-kobo")
+info = netrc.netrc(homedir + "/.netrc")
 username, account, password = info.authenticators(host)
 
 filelist = glob.glob("tempfiles/*.xml")
 
 filelist_tuples = [(f, open(f, 'rb')) for f in filelist]
-filelist_tuples = (filelist_tuples)
+filelist_tuples = tuple(filelist_tuples)
 
 logdata = []
 logfilename = datetime.now().strftime('postlog__%Y-%m-%d_%H-%M.csv')
@@ -23,13 +24,23 @@ for ft in filelist_tuples:
     files = {'xml_submission_file': ft}
 
     response = requests.post('https://kc.kobotoolbox.org/submission', files=files, auth=(username, password))
-    logdata.append(response)
+
+    d = {}
+    d['res'] = response
+    # get uuid from filepath in files tuple
+    d['uuid'] = re.split(r'[\/.]', ft[0])[1]
+    logdata.append(d)
 
 
 with open("kc"+logfilename, 'w') as csvfile:
-    fieldnames = ['code', 'response_text', 'date']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+    fieldnames = ['code', 'response_text', 'date', 'uuid']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
     writer.writeheader()
-    for r in logdata:
-        writer.writerow({'code': r.status_code, 'response_text': r.text, 'date': r.headers['Date']})
+
+    for ld in logdata:
+        writer.writerow({
+            'code':          ld['res'].status_code,
+            'response_text': ld['res'].text,
+            'date':          ld['res'].headers['Date'],
+            'uuid':          ld['uuid']
+        })
